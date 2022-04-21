9f35dc48588472ba51a07305e7c981780820d521
t-vedgupta <t-vedgupta@microsoft.com>
2022-04-21 09:26:45 +0530

    works

--- src/App.js
+++ src/App.js
@@ -35,7 +35,8 @@ function clickButton  () {
     // }, 10000);
     // goto('https://m.badoo.com/aa/landto/?ref=abcdef&page=9&user_id=1234567','instagram://');
     // goto('https://mlink.badoo.com/aa/landto/?ref=abcdef&page=9&user_id=1234567','https://bdo.to/u/abcdef');
-    goto('https://bdo.to/u/abcdef','instagram://');
+    // goto('https://bdo.to/u/abcdef','instagram://');
+    goto('badoo://user/1234567','instagram://');
     
     
   } else {
