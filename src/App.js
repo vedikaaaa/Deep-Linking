6eea33f7d39bf7b22f0227013c3a9f249a6df713
t-vedgupta <t-vedgupta@microsoft.com>
2022-05-24 16:47:17 +0530

    learn react

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import "./App.css";
 
 export default function App() {
   const [pdfSupport, _] = useState(() => navigator.pdfViewerEnabled);
+  const [pdfSupportt,__] = useState(() => navigator.plugins);
   return (
     <div className="App">
       <h1>PDF View Support</h1>
       <h2>{pdfSupport ? "Supported" : "Not Supported"}</h2>
+      <h2>{pdfSupportt}</h2>
     </div>
   );
 }
